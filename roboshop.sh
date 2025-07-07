c7c8ff7b7f87b7de8c411d08cfcc5dcebca5c9e9
Suraj Padhi <surajpadhi4546@gmail.com>
2025-07-07 19:56:40 +0530

    Shell script

--- roboshop.sh
+++ roboshop.sh
@@ -10,17 +10,36 @@ DOMAIN_NAME="skptech.site"
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance != "frontend" ]
+    if [ "$instance" != "frontend" ]
     then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME"
-    else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        RECORD_NAME="$DOMAIN_NAME"
-    fi
-    echo "$instance IP address: $IP"
-
+        # Wait for Private IP to be assigned
+        while true; do
+            IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+            if [[ "$IP" != "None" && -n "$IP" ]]; then
+                break
+            fi
+            echo "Waiting for $instance to get Private IP..."
+            sleep 5
     aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '{
+        "Comment": "Creating or Updating a record set for cognito endpoint",
+        "Changes": [
+            {
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": "'$RECORD_NAME'",
+                    "Type": "A",
+                    "TTL": 1,
+                    "ResourceRecords": [
+                        {
+                            "Value": "'$IP'"
+                        }
+                    ]
+                }
+            }
+        ]
+    }'
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
