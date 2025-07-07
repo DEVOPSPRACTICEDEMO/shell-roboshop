de7e26c424bc0436ace6610f36e112db7f1e1fdb
Suraj Padhi <surajpadhi4546@gmail.com>
2025-07-07 19:37:36 +0530

    Shell script

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,7 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-050db45fd22113a83"
 INSTANCES=("mongodb" "redis" "catalogue" "user" "cart" "shipping" "payment" "frontend" "dispatch" "rabbitmq" "mysql")
-ZONE_ID= "Z07092431FXHTW58PX8NM"
+ZONE_ID="Z07092431FXHTW58PX8NM"
 DOMAIN_NAME="skptech.site"
 
 for instance in ${INSTANCES[@]}
@@ -29,11 +29,9 @@ do
             ,"Type"         : "A"
             ,"TTL"          : 1
             ,"ResourceRecords" : [{
-                "Value"     : "'$IP'"
+            "Value"     : "'$IP'"
             }] 
         }
         }]
     }'
 done
-
-
