67e1d11804e841f246d04088feeb32a60201742c
Suraj Padhi <surajpadhi4546@gmail.com>
2025-07-08 13:29:02 +0530

    Shell script

--- redis.sh
+++ redis.sh
@@ -41,3 +41,18 @@ VALIDATE $? "Enabling Redis 7 Version"
 
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Edited redis.conf to accept remote connections"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enable Redis"
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Started Redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script executed successfully, $Y time taken: $TOTAL_TIME $N" | tee -a $LOG_FILE
+
+
