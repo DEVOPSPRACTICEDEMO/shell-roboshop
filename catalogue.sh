9b96dafb43b2c1bb44cdc9fc8e6642b1d440124f
Suraj Padhi <surajpadhi4546@gmail.com>
2025-07-08 12:20:25 +0530

    Shell script

--- catalogue.sh
+++ catalogue.sh
@@ -43,7 +43,7 @@ dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing nodejs:20"
 
 id roboshop
-if [$? -ne 0 ]
+if [$? -ne 0 ];
 then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
     VALIDATE $? "Creating roboshop system user"
@@ -78,7 +78,7 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
 STATUS=$(mongosh --host mongodb.skptech.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-if [$STATUS -lt 0 ]
+if [$STATUS -lt 0 ];
 then
     mongosh --host mongodb.skptech.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
